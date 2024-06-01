db22d383f9eb7db69177119db0d21e1cdbafafb0
Oleksii Bielikoff <aabelikoff@gmail.com>
2024-06-01 14:39:10 +0100

    Add production build files

--- src/db/minionCards.js
+++
@@ -1,8 +0,0 @@
-import { importMinionsImages } from "../utills/imagesImporter";
-
-const allImages = importMinionsImages();
-const cardsInfo = allImages.map((image, index) => {
-  return { image, id: index };
-});
-
-export default cardsInfo;
--- src/utills/imagesImporter.js
+++
@@ -1,9 +0,0 @@
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-function importMinionsImages() {
-  return importAll(require.context("../images/minions_bg_free", false, /\.(png|jpe?g|svg)$/));
-}
-
-export { importAll, importMinionsImages };
